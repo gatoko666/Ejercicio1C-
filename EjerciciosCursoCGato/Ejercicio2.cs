ea4ea0494d27bd936bc9aca68b7f2ba1f4b7a3e4
gatoko <g.millaquiensuarez@gmail.com>
2018-02-28 22:56:08 -0300

    Inicio proyecto

---
+++ EjerciciosCursoCGato/Ejercicio2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2) Escribir un método que acepte un número y devuelva True si el número es par, o false si es impar.
+
+namespace EjerciciosCursoCGato
+{
+    class Ejercicio2
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Escriba un número: ");
+            int numero1 = int.Parse(Console.ReadLine());
+
+            if (numero1%2==0)
+            {
+                Console.WriteLine("El número es par ");
+
+            }
+            else 
+            {
+                Console.WriteLine("El número es impar ");
+
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ EjerciciosCursoCGato/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+//Permitir al usuario escribir dos números y mostrar en pantalla cuál de los dos es el más grande.Si son iguales, mostrar cualquiera.
+
+
+namespace EjerciciosCursoCGato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                try
+                {
+                    Console.WriteLine("Escriba un número: ");
+                    int numero1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Escriba otro  número: ");
+                    int numero2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("El número más grande es :");
+
+                    if (numero1 > numero2)
+                    {
+                        Console.WriteLine(numero1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(numero2);
+                    }
+
+                    break;
+
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Escibre un número XD ");
+                }
+            }
+
+            Console.Read();
+
+        }
+    }
+}
